13c6e45af3b64465b08efc25178d33196814e9b5
Tsi <tsilune@gmail.com>
2018-10-17 17:03:10 +0200

    zv

---
+++ README.md
@@ -0,0 +1,6 @@
+# Modal
+
+##Couleurs
+
+rgba (0, 0, 0, 0) > transparent
+rgba (0, 0, 0, 1) > noir
---
+++ js/main.js
@@ -0,0 +1,8 @@
+var test = document.querySelector('.test');
+var modal = document.querySelector('.modal');
+
+test.addEventListener('click', function(e) {
+	modal.classList.add('visible');
+});
+
+// var bouton = document.querySelector('');
